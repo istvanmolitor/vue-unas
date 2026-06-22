d8550294d8b44e08027b7713c03f986b3f735f07
Molitor István <istvanmolitor@gmail.com>
2026-06-22 14:07:23 +0200

    id

--- views/UnasProductParameterIndex.vue
+++ views/UnasProductParameterIndex.vue
@@ -25,7 +25,6 @@ const pagination = ref<PaginationMeta>({
 })
 
 const columns: Column<UnasProductParameter>[] = [
-  { key: 'id', label: 'ID', sortable: true, width: '80px' },
   { key: 'name', label: 'Név', sortable: true },
   { key: 'type', label: 'Típus', sortable: true },
   { key: 'language_name', label: 'Nyelv', sortable: false },
