b0a9ec123fb39c49b580a9578267e311e077a649
Molitor István <istvanmolitor@gmail.com>
2026-06-28 04:31:47 +0200

    data table

--- services/unasService.ts
+++ services/unasService.ts
@@ -103,6 +103,7 @@ export interface PaginatedResponse<T> {
     per_page: number
     total: number
   }
+  columns?: Array<{ key: string; label: string; sortable: boolean }>
 }
 
 export interface SingleResponse<T> {
